a9d0457cb9c2506831446768dc08a97ddf32b981
Sudipta Jana <sjgalaxy98@gmail.com>
2022-03-10 16:58:02 +0530

    first commit

---
+++ resources/views/backend/includes/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="app-footer">
+        <div class="d-block w-100 text-center"><strong>@lang('labels.general.copyright') &copy; {{ date('Y') }} All_rights_reserved <p class="float-right mb-0">Powered By <a href="#" target="_blank" class="mr-4"> LMS</a></p></div>
+
+</footer>
